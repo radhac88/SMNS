07de1754eccbecd1b7d4f43edafa42ea2b0531d0
Shiva Prasad <shivaprasad@etggs.net>
2018-07-30 10:28:50 +0530

    exercises

---
+++ shiva/python exercises/fact.py
@@ -0,0 +1,9 @@
+import math
+for i in range(1,11):
+        x = math.factorial(i);
+        if(x >100 and x<=1000):
+                print("the number lies between 1 to 1000:", i)
+        elif(x >1000 and x<10000):
+                print("the number lies between 1000 to 2000:",i)
+        
+        
