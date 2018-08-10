a7595f3e89ae82f4ac13ee3dca2ec019c5fdf277
Nikhila reddy <nikhila.reddy@etggs.net>
2018-08-10 14:30:30 +0530

    'practice'

--- nikhila/iteration.py
+++ nikhila/iteration.py
@@ -148,3 +148,15 @@ def new_string(str):
 
 print(new_string("Array"))
 print(new_string("IsEmpty"))
+
+#ex
+def fahrenheit(T):
+    return ((float(9)/5)*T + 32)
+def celsius(T):
+    return (float(5)/9)*(T-32)
+temp = (36.5, 37, 37.5,39)
+
+F = list(map(fahrenheit, temp))
+print(F)
+C = list(map(celsius, F))
+print(C)
