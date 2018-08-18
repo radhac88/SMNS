0dcbec0f8447ac67a678bf1e7a9e10882d038def
Shiva Prasad <shivaprasad@etggs.net>
2018-08-18 15:13:26 +0530

    added some js

--- shiva/task/main.js
+++ shiva/task/main.js
@@ -1,71 +1,101 @@
-$(document).ready(function(){
-    $.ajax({url: "http://localhost:1111", success: function(result){
-        $("#data-grid").css("display","block")
-        	for(var i=0; i<result.length ;i++){
-                var clone = $("#data-grid").clone();
-                clone.find("#showimg").attr("src",result[i].show_key_art);
-                clone.find("#showtitle").html(result[i].title);
-                clone.find("#des").html(result[i].synopsis["short-synopsis"]);
-                clone.find("#showlang").html(result[i].audio);
-                clone.find("#showrating").html(result[i].ratings);
-                clone.find(".seemore").data('synop', result[i].synopsis["long-synopsis"]);
-                clone.find(".seemore").data('msynop', result[i].synopsis["medium-synopsis"]);
-                 $("#image-container").append(clone);
-                for(var j=0; j<result[i].videos.length ;j++){
-                    $("#image-container").append('<div id=video-container class="col-sm-6"><img src="'+result[i].videos[j].video_thumbnail+'"/></div>'
-                        +'episode:'+result[i].videos[j].episode_title+'<br>'
-                        +'Ratings:'+result[i].videos[j].ratings+'<br>' 
-                        +'season:'+result[i].videos[j].season +'<br>');
+$(document).ready(function () {
+    var movies = [];
+     var count = 10;
+    function myCount() {
+    if (count < 0) {
+        $('.count').text(" ");
+
+        }
+    else{
+        $('.count').text(count);
+                count --;
+            }
+             setInterval(myCount,500);
     }
-           /* for(var j=0; j<result[i].videos.length ;j++){
-                $(".btn").click(function(){
-            $(".container1").append('<div class="row">'
-            +'<p>epsiode title:</p>'+result[i].videos[j].episode_title
-            +result[i].videos[j].video_thumbnail
-            +'<p>season</p>'+result[i].videos[j].season 
-            +'<p>ratings</p>'+result[i].videos[j].ratings+'</div>');
-               
-                });
-             }*/       
-           
        
-         	}
-            $(".seemore").click(function(){
-                var synop = $(this).data("synop");
-                if(synop == "undefined"){
-                    synop = $(this).data("msynop");
+
+    // start loading
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:1111',
+        success: function (response) {
+            movies = response;
+        },
+        complete: function () {
+            $('#pagination').twbsPagination({
+                totalPages: movies.length / 10,
+                visiblePages: 10,
+                onPageClick: function (event, page) {
+                    $("#page-content").html("");
+                    let startIndex = (page - 1) * 10;
+                    let endIndex = (page * 10) - 1;
+                    for (var i = startIndex; i < endIndex; i++) {
+                        $("#page-content").append('<div class="row"><div class="col-sm-3"><img src="'
+                            + movies[i].show_key_art + ' " onerror="imgError(this);" /></div><div class="col-sm-9"><b>Title:</b>"'
+                            + movies[i].title + ' "<p id="des"><b>Description:</b>"'
+                            + movies[i].synopsis["short-synopsis"]
+                            + "...<span data-toggle ='modal' data-target= '#myModal'class='seemore' data-synop='" 
+                            + movies[i].synopsis["full-synopsis"] + "'data-mysynop = '" 
+                            + movies[i].synopsis["medium-synopsis"] + "' class='btn-default'>seemore</span>"
+                            + '"<p><b>language:</b></p>"'
+                            + movies[i].audio + ' "<p><b>Ratings:</b>"'
+                            + movies[i].ratings
+                            +'<span id="ldc"></span>'
+                            + '"</p><button id="loadmore">load more</button></div></div>'
+                            );
+                        $('#loadmore').click(function() {
+                        $('.menu').toggle();
+                                    });
+                        for(var j= i; j<movies[i].videos.length;j++){
+                            $("#idc").append('<div class="menu"><p><b>episode title</p></b><p>'+movies[i].videos[j].episode_title+'</p>'
+                                +'<p><b>season</p></b><p>'+movies[i].videos[j].season+'</p>'
+                                +'<p><b>ratings</p></b><p>'+movies[i].videos[j].ratings+'</p>'
+                                +'<p><b>thumbnail</p></b><img src="'+movies[i].videos[j].video_thumbnail+'" width="100px; height="100px";/><br></div>');
+                            
+                      } 
+                        /*for(var j=0; j<movies[i].videos.length ;j++){
+                            $("#page-content").append(movies[i].videos[j].episode_title+ '----' +movies[i].videos[j].season +'<br>');
+                        }*/
+                        $("#page-content").append("<div><hr></div>");
+                    }
+                    $(".seemore").click(function () {
+                        var synop = $(this).data("synop");
+                        if (synop == "undefined") {
+                            synop = $(this).data("mysynop");
+                        }
+                        var rep = synop.replace(/[\u21b5\u21e5]/g, " ")
+                        $("#full-synop").html(rep);
+                    });
                 }
-                $("#full-synop").html(synop);
+            }).on('page', function (event, page) {
+                // console.info(page + ' (from event listening)');
             });
-
-        
-    }});
-});
- /*for(var j=0; j<result[i].videos.length ;j++){
-        $("#image-container").append(result[i].videos[j].episode_title+ '----' +result[i].videos[j].season +'<br>');
-    }
-    for(var j=0; j<result[i].videos.length ;j++){
-        var vclone = $("#videos-grid").clone();
-        vclone.find("#.loadmore").toggle(1000).data('title',result[i].videos[j].episode_title);
-        vclone.find("#ratings").data(result[i].videos[j].ratings);
-        vclone.find("#season").data(result[i].videos[j].season);  
-        $("#image-container").append(vclone);
-    }
-    */
+        }
+    });
+});//ready func close
 
 
-     
+$(document).load(function () {
+
+})
+
 //scroll function
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        document.getElementById("myBtn").style.display = "block";
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topBtn").style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("topBtn").style.display = "none";
     }
 }
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+// for dummy image
+function imgError(image) {
+    image.onerror = "";
+    image.src = "./images/inf.jpg";
+    return true;
 }
