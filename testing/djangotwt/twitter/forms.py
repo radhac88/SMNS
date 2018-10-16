c02087bd82dd202db7a8a6793703288aede8ed74
Sai Manaswini <sai.manaswini@etggs.net>
2018-10-16 12:49:48 +0530

    testing

---
+++ testing/djangotwt/twitter/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Signup
+
+class UserForm(forms.ModelForm):
+	confirmpassword = forms.CharField(widget=forms.PasswordInput())
+	password = forms.CharField(widget=forms.PasswordInput())
+
+	class Meta:
+		model = Signup
+		fields = ('username', 'phonenumber','email','password','confirmpassword',)
+
+
+class LoginForm(forms.ModelForm):
+	
+	class Meta:
+		model = Signup
+		fields = ('username','password',)
---
+++ testing/djangotwt/twitter/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+class Signup(models.Model):
+    username = models.CharField(max_length=30, blank=True, null=True)
+    phonenumber=models.CharField(max_length=10,default=None, unique=True)
+    email = models.EmailField(max_length=254, unique=True, db_index=True, primary_key=True, default=None, blank=True)
+    password = models.CharField(max_length=15, default=None, unique=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.username
+
+# Create your models here.
+
