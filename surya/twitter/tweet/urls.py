fa21e501f276660d21378e1a5a5dcbf027b343bf
Surya Kala <surya.kala@etggs.net>
2018-10-23 17:49:23 +0530

    twitter

--- surya/twitter/tweet/urls.py
+++ surya/twitter/tweet/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path('', views.start, name='start'),
     path('signup/',views.signup,name='signup'),
     path('home/',views.home,name='home'),
-    path('login/',views.login,name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('login/',views.user_login,name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('search/', views.search, name='search'),
 ]
