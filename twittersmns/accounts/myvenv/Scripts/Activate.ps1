a6f24238ce38596a7016c72116af16420cd743b7
Surya Kala <surya.kala@etggs.net>
2018-11-30 11:24:03 +0530

    New twitter

--- twittersmns/accounts/myvenv/Scripts/Activate.ps1
+++ twittersmns/accounts/myvenv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\gitfiles\SMNS\twitter\accounts\myvenv"
+$env:VIRTUAL_ENV="D:\SMNS\twittersmns\accounts\myvenv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
